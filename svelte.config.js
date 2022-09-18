794af9ec4e56a13d5dfb037560cc58be95cec500
RomainSav <romain.sv@icloud.com>
2022-09-18 20:22:36 +0200

    Add markdown rendering in posts

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true
-	}),
+  extensions: [".svelte"],
 
-	kit: {
-		adapter: adapter(),
-		alias: {
-			'@components/*': 'src/lib/components/',
-			'@css/*': 'src/lib/css/'
-		}
-	}
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true,
+    })
+  ],
+
+  kit: {
+    adapter: adapter(),
+    alias: {
+      "@components/*": "src/lib/components/",
+      "@css/*": "src/lib/css/",
+    },
+  },
 };
 
 export default config;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,11 @@
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, '$1')
+    .replace(/\.0$/, '')
+const rem = (px) => `${round(px / 16)}rem`
+const em = (px, base) => `${round(px / base)}em`
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -15,10 +23,43 @@ module.exports = {
         primary: '#5339DD',
         darkprimary: '#38259C',
         lightprimary: '#7F69F2'
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            img: {
+              width: '95%',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              borderRadius: '0.375rem'
+            }
+          }
+        },
+        invert: {
+          css: {
+            '--tw-prose-body': 'var(--tw-prose-invert-body)',
+            '--tw-prose-headings': 'var(--tw-prose-invert-headings)',
+            '--tw-prose-lead': 'var(--tw-prose-invert-lead)',
+            '--tw-prose-links': 'var(--tw-prose-invert-links)',
+            '--tw-prose-bold': 'var(--tw-prose-invert-bold)',
+            '--tw-prose-counters': 'var(--tw-prose-invert-counters)',
+            '--tw-prose-bullets': 'var(--tw-prose-invert-bullets)',
+            '--tw-prose-hr': 'var(--tw-prose-invert-hr)',
+            '--tw-prose-quotes': 'var(--tw-prose-invert-quotes)',
+            '--tw-prose-quote-borders': '#5339DD',
+            '--tw-prose-captions': 'var(--tw-prose-invert-captions)',
+            '--tw-prose-code': 'var(--tw-prose-invert-code)',
+            '--tw-prose-pre-code': 'var(--tw-prose-invert-pre-code)',
+            '--tw-prose-pre-bg': 'var(--tw-prose-invert-pre-bg)',
+            '--tw-prose-th-borders': 'var(--tw-prose-invert-th-borders)',
+            '--tw-prose-td-borders': 'var(--tw-prose-invert-td-borders)',
+          }
+        }
       }
     },
   },
   plugins: [
+    require('@tailwindcss/typography'),
     require('@tailwindcss/line-clamp')
   ],
 }
