6b9445e77736a9061d06b08578cdb31aec2cea61
RomainSav <romain.sv@icloud.com>
2022-09-20 20:34:24 +0200

    Adjust Sidebar

--- src/lib/components/layouts/sidebar/SidebarGroup.svelte
+++ src/lib/components/layouts/sidebar/SidebarGroup.svelte
@@ -39,9 +39,9 @@
 </script>
 
 <div class="border-b border-lightprimary mb-4 pb-4">
-    <div class="flex justify-between items-center mb-2">
+    <div on:click={toggle} class="flex justify-between items-center mb-2 cursor-pointer">
         <h3 class="uppercase tracking-widest text-neutral-200 font-semibold text-sm">{title}</h3>
-        <svg on:click={toggle} class="fill-neutral-200 w-3 h-3 cursor-pointer transition-transform duration-300" class:rotate-180={$settings.closedSidebarMenus?.includes(id)} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 30.727 30.727" style="enable-background:new 0 0 30.727 30.727;" xml:space="preserve">
+        <svg class="fill-neutral-200 w-3 h-3 transition-transform duration-300" class:rotate-180={$settings.closedSidebarMenus?.includes(id)} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 30.727 30.727" style="enable-background:new 0 0 30.727 30.727;" xml:space="preserve">
             <g>
                 <path d="M29.994,10.183L15.363,24.812L0.733,10.184c-0.977-0.978-0.977-2.561,0-3.536c0.977-0.977,2.559-0.976,3.536,0
                     l11.095,11.093L26.461,6.647c0.977-0.976,2.559-0.976,3.535,0C30.971,7.624,30.971,9.206,29.994,10.183z"/>
