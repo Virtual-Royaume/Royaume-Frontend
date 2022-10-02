35e541c830b2a21a89dd8d9b13f5aa80c127dbb3
RomainSav <romain.sv@icloud.com>
2022-10-02 12:00:42 +0200

    Update MD

--- src/lib/components/elements/markdown/editor/MdEditor.svelte
+++ src/lib/components/elements/markdown/editor/MdEditor.svelte
@@ -1,41 +1,71 @@
 <script lang="ts">
-    import { marked } from 'marked';
-    import { element } from 'svelte/internal';
+    import { fromMarkdown } from "mdast-util-from-markdown";
+    import type { Content } from "mdast";
 
     let inputRef: HTMLDivElement;
     let value: string;
 
     const onType = (event: any) => { 
-        const children = marked.lexer(event.target.innerText);
+        event.preventDefault();
+        const children = fromMarkdown(event.target.innerText).children;
 
-        console.log(children)
 
         let content = '';
-        children.forEach((child: marked.Token) => {
+        children.forEach((child: Content) => {            
             content += getTag(child);
-        })
+            content += '<p><br /></p>'
+        });
 
+        event.target.innerHTML = content;
     }
 
-    const getTag = (element: marked.Token): string => {
-        if (element.type === 'heading') {
-            if (element.depth === 1) {
-                return '<span class="font-bold text-2xl">' + element.raw + '</span>';
+    const getCleanText = (txt: string): string => {
+        let sanitizedText = txt.replace(/^\s*\n/gm, '<p><br /></p>');
+
+        sanitizedText = sanitizedText.replace(/<\/?span[^>]*>/g, '');
+
+        sanitizedText =  sanitizedText.replace(/class='[a-zA-Z0-9:;\\.\\s\\(\\)\\-\\,]*'/g, '');
+
+        return sanitizedText;
+    }
+
+
+    const onPaste = (event: ClipboardEvent) => {
+        event.preventDefault();
+
+        const pastedText = event.clipboardData?.getData('text');
+        if (!pastedText) return;
+
+        document.execCommand('insertHTML', false, getCleanText(pastedText));
+    }
+
+    const getTag = (element: Content, parent: Content | null = null): string => {
+        if (parent) {
+            if (parent.type === 'heading' && element.type === 'text') {
+                if (parent.depth === 1) {
+                    return '<span class="font-bold text-2xl">' + element.value + '</span>';
+                } else {
+                    return '<span class="font-bold text-xl">' + element.value + '</span>';
+                }
             }
-            if (element.depth === 2) {
-                return '<span class="font-bold text-xl">' + element.raw + '</span>';
+
+            return '';
+        } else {
+            if (element.type === 'heading' || element.type === 'blockquote') {
+                return getTag(element.children[0], element);
             }
-            return '<span class="font-bold text-2xl">' + element.raw + '</span>';
+            if (element.type === 'paragraph') {
+                return (
+                    '<span>' + 
+                        element.children.map((child) => getTag(child, element))
+                    + '</span>'
+                );
+            }
+
+            return '';
         }
-
-        if (element.type === 'space') {
-            return '<br>'
-        }
-
-
-        return element.raw;
     }
 </script>
 
-<div bind:this={inputRef} bind:textContent={value} contenteditable on:input={onType} class="w-full text-neutral-400 outline-none resize-none bg-neutral-800 p-2 rounded-b-md border border-neutral-600 focus:border-lightprimary">
+<div bind:this={inputRef} bind:textContent={value} contenteditable on:paste={onPaste} on:input={onType} class="w-full text-neutral-400 outline-none resize-none bg-neutral-800 p-2 rounded-b-md border border-neutral-600 focus:border-lightprimary">
 </div>
