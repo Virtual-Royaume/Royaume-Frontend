41009324fc5c1da5173ddcf4744e90a83fb828f9
RomainSav <romain.sv@icloud.com>
2022-10-02 19:05:19 +0200

    Initial commit

--- src/lib/components/layouts/typography/SubHeading.svelte
+++
@@ -1,3 +0,0 @@
-<h2 class="text-2xl text-neutral-400 font-medium">
-    <slot></slot>
-</h2>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,56 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        xl: '900px',
-        '2xl': '1420px'
-      }
-    },
-    extend: {
-      colors: {
-        primary: '#5339DD',
-        darkprimary: '#38259C',
-        lightprimary: '#7F69F2'
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            img: {
-              marginLeft: 'auto',
-              marginRight: 'auto',
-              borderRadius: '0.375rem',
-            }
-          }
-        },
-        invert: {
-          css: {
-            '--tw-prose-body': 'var(--tw-prose-invert-body)',
-            '--tw-prose-headings': 'var(--tw-prose-invert-headings)',
-            '--tw-prose-lead': 'var(--tw-prose-invert-lead)',
-            '--tw-prose-links': 'var(--tw-prose-invert-links)',
-            '--tw-prose-bold': 'var(--tw-prose-invert-bold)',
-            '--tw-prose-counters': 'var(--tw-prose-invert-counters)',
-            '--tw-prose-bullets': 'var(--tw-prose-invert-bullets)',
-            '--tw-prose-hr': 'var(--tw-prose-invert-hr)',
-            '--tw-prose-quotes': 'var(--tw-prose-invert-quotes)',
-            '--tw-prose-quote-borders': '#5339DD',
-            '--tw-prose-captions': 'var(--tw-prose-invert-captions)',
-            '--tw-prose-code': 'var(--tw-prose-invert-code)',
-            '--tw-prose-pre-code': 'var(--tw-prose-invert-pre-code)',
-            '--tw-prose-pre-bg': 'var(--tw-prose-invert-pre-bg)',
-            '--tw-prose-th-borders': 'var(--tw-prose-invert-th-borders)',
-            '--tw-prose-td-borders': 'var(--tw-prose-invert-td-borders)',
-          }
-        }
-      }
-    },
+    extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp')
-  ],
+  plugins: [],
 }
