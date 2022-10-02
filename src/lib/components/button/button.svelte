f10209a547f3fb0417e7ed9d2725d5991e29f16f
RomainSav <romain.sv@icloud.com>
2022-10-02 23:46:13 +0200

    Finish Unit Test setup + test exemple

---
+++ src/lib/components/button/button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let name: string;
+
+	let text = "Button";
+
+	const handleClick = () => {
+		text = "Button Clicked";
+    }
+</script>
+
+<h1>Hello {name}</h1>
+
+<button on:click={handleClick}>{text}</button>
