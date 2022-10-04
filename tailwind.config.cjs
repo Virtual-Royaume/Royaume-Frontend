9b7130f5b403fac1f14da9849e6fbc4926470c62
Bluzzi <camilledugas.pro@gmail.com>
2022-10-04 16:14:22 +0200

    Tailwind: add font size and color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,29 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   purge: false,
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#5339DD",
+
+        "white": "white",
+        "black": "black",
+
+        "gray": "#F2F2F2",
+
+        "red": "#FF5555",
+        "green": "#27ae60"
+      },
+      fontSize: { // TODO : use REM
+        small: "14px",
+        normal: "1rem",
+        large: "48px",
+        xl: "34px",
+        "2xl": "60px",
+        "3xl": ["80px", { lineHeight: 1 }]
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
