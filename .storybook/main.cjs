6f4bca02544d366272560f39613d2d51d485902e
Bluzzi <camilledugas.pro@gmail.com>
2022-10-04 03:06:31 +0200

    Package: clean packages

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -13,7 +13,7 @@ module.exports = {
       name: '@storybook/addon-postcss',
       options: {
           postcssLoaderOptions: {
-              implementation: require('postcss'),
+              implementation: require("postcss"),
           },
       },
     }
