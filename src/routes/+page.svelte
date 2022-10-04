3fb9eede8e565092400f4f44472bd111856e8383
Bluzzi <camilledugas.pro@gmail.com>
2022-10-04 17:37:08 +0200

    / (page): push test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
   import { Button } from "$lib/components/navigation/button";
+  import { Text } from "$lib/components/texts/text";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <Button>Click me</Button>
+
+<Text color="primary">Test</Text>
+<p class="text-red text-3xl">Test</p>
