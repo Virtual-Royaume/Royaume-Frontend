63e8edbdd7090152dd5984e6fb6559b55bf1d837
Bluzzi <camilledugas.pro@gmail.com>
2022-10-04 02:31:18 +0200

    Clean UP: useless files, ESLint, folder structure...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+  import { Button } from "$lib/components/navigation/button";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button>Click me</Button>
