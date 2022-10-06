c1902a7e4a685a356657f82e696fef6d2a5395fa
RomainSav <romain.sv@icloud.com>
2022-10-06 21:49:55 +0200

    Add TextArea component

---
+++ src/libraries/components/form/text-area/text-area.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { clsx } from "clsx";
+  import { Text } from "../../texts/text";
+  import type { TextAreaType, TextAreaColor, TextAreaSize } from "./text-area.type";
+  
+  // Props
+  export let color: TextAreaColor = "primary";
+  export let label: string | null = null;
+  export let placeholder: string | null;
+  export let type: TextAreaType = "text";
+  export let autofocus: boolean = false;
+  export let required: boolean = false;
+  export let value: string | null = null;
+  export let disabled: boolean = false;
+  export let size: TextAreaSize = "normal";
+  export let autoResize: boolean = true;
+  export let rows: number = 1;
+  
+  // Refs :
+  let textAreaRef: HTMLTextAreaElement;
+  
+  onMount(() => autofocus && textAreaRef.focus());
+
+  const resize = (): void => {
+    value = textAreaRef.value;
+  }
+
+  $: rows = autoResize ? (value?.match(/\n/g) || []).length + 1 || 1 : rows;
+  
+  // Style :
+  $: style = clsx("shadow outline-none focus:ring bg-gray-1 border border-gray-2 rounded-md text-normal w-full", {
+    // Ring :
+    "focus:ring-primary-1/50": color === "primary",
+    "focus:ring-secondary-1/50": color === "secondary",
+    "focus:ring-success-1/50": color === "success",
+    "focus:ring-warning-1/50": color === "warning",
+    "focus:ring-error-1/50": color === "error",
+  
+    // Size :
+    "px-2 py-1": size === "small",
+    "p-2": size === "normal",
+    "px-2 py-4": size === "large",
+
+    // Resize
+    "overflow-hidden resize-none": autoResize
+  });
+</script>
+
+<label>
+  {#if label}
+    <div class="mb-1">
+      <Text type="p" size="normal" fontWeight="medium">{label}</Text>
+    </div>
+  {/if}
+  <textarea bind:this={textAreaRef} on:input={resize} {placeholder} {type} {rows} {required} {disabled} class={style}></textarea>
+</label>
