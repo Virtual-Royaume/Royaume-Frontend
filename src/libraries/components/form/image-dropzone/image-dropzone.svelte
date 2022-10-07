e4048627985693a254a3d14b84d5f5231a2f8e70
RomainSav <romain.sv@icloud.com>
2022-10-07 17:45:09 +0200

    Edit ImageDropzone

--- src/libraries/components/form/image-dropzone/image-dropzone.svelte
+++ src/libraries/components/form/image-dropzone/image-dropzone.svelte
@@ -2,17 +2,19 @@
   import { clsx } from "clsx";
   import { FileUploadIcon } from "../../../icons";
   import type { ImageDropzoneColor, ImageDropzoneSize } from "./image-dropzone.type";
+  import ImageDropzonePreview from "./image.dropzone.preview.svelte";
   import Text from "../../texts/text/text.svelte";
   import type { FileType } from "../../../utils/types";
 
   // Props :
   export let multiple: boolean = false;
   export let color: ImageDropzoneColor = "primary";
-  export let preview: boolean = false;
+  export let preview: boolean = true;
   export let size: ImageDropzoneSize = "normal";
   export let accepted: FileType[] = ["image/png"];
 
   let isHover: boolean = false;
+  let files: File[] = [];
 
   // Style :
   $: style = clsx("flex flex-col hover:bg-gray-2 transition-colors cursor-pointer border border-gray-2 justify-center items-center p-2 rounded-md shadow", {
@@ -34,29 +36,30 @@
   });
 
   const upload = (event: DragEvent) => {
-    let files: File[] = [];
-
     if (event.dataTransfer?.files) {
       [...event.dataTransfer.files].forEach((file, i) => {
         if (accepted.length > 0 && (!file.type || !accepted.includes(<FileType>file.type))) return;
         files = [...files, file];
       });
     }
-
-    console.log(files);
   }
 </script>
 
-<label class={style}
+<div>
+  <label class={style}
   on:dragover|preventDefault={() => isHover = true}
   on:dragleave={() => isHover = false}
   on:drop|preventDefault={upload}
->
-  <input type="file"class="hidden">
+  >
+    <input type="file"class="hidden">
 
-  <FileUploadIcon size={40} color="black" />
-  <div class="text-center mt-4">
-    <Text size="normal">Cliquer ou gilsser et déposer pour télécharger</Text>
-  </div>
+    <FileUploadIcon size={40} color="black" />
+    <div class="text-center mt-4">
+      <Text size="normal">Cliquer ou gilsser et déposer pour télécharger</Text>
+    </div>
+  </label>
 
-</label>
+  {#if preview}
+    <ImageDropzonePreview {files} />
+  {/if}
+</div>
---
+++ src/libraries/components/form/image-dropzone/image.dropzone.preview.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  // Props :
+  export let files: File[] = [];
+
+  $: datas = files.map((file) => {
+    return URL.createObjectURL(file);
+  });
+
+
+</script>
+
+{#if files.length > 0}
+  <div class="flex gap-2 overflow-x-scroll mt-4">
+    {#each datas as data, i}
+      <img src={data.toString()} alt={i + " Preview"} class="h-24 rounded-md">
+    {/each}
+  </div>
+{/if}
