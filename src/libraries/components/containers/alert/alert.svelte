3a7558d78b48365dff0667ad8c0f440f1d65f517
RomainSav <romain.sv@icloud.com>
2022-10-08 12:07:43 +0200

    Introduce Alert component

---
+++ src/libraries/components/containers/alert/alert.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { Text } from "../../texts/text";
+  import { clsx } from "clsx";
+  import type { AlertType } from "./alert.type";
+
+  // Props :
+  export let type: AlertType = "success";
+  export let message: string;
+
+  // Style :
+  const style = clsx("flex items-center gap-2 p-4 shadow-sm rounded-md bg-success-1/10");
+</script>
+
+<div class={style}>
+  {#if type === "success"}
+
+  {/if}
+  {#if type === "warning"}
+    
+  {/if}
+  {#if type === "error"}
+    
+  {/if}
+  <Text>{message}</Text>
+</div>
