f85457f6cfc4c960c786a60ac8309cd7f5e81e8d
RomainSav <romain.sv@icloud.com>
2022-10-11 22:05:04 +0200

    Update form validation

--- src/libraries/components/form/input/input.svelte
+++ src/libraries/components/form/input/input.svelte
@@ -16,15 +16,18 @@
   export let disabled = false;
   export let size: InputSize = "normal";
   export let name: string | null = null;
-  export let validators: {(): boolean}[] = [];
+  export let validators: {(value: string | number | null, required: boolean): string}[] = [];
 
   // Refs :
   let inputRef: HTMLInputElement;
 
-  onMount(() => autofocus && inputRef.focus());
+  onMount(() => {
+    inputRef.type = type;
+    if (autofocus) inputRef.focus();
+  });
 
   // Style :
-  $: style = clsx("shadow outline-none focus:ring bg-gray-1 border border-gray-2 rounded-md text-normal w-full", {
+  $: style = clsx("shadow outline-none focus:ring bg-gray-1 border rounded-md text-normal w-full", {
     // Ring :
     "focus:ring-primary-1/50": color === "primary",
     "focus:ring-secondary-1/50": color === "secondary",
@@ -35,12 +38,25 @@
     // Size :
     "px-2 py-1": size === "small",
     "p-2": size === "normal",
-    "px-2 py-4": size === "large"
+    "px-2 py-4": size === "large",
+
+    "border-error-1": error.length > 0,
+    "border-gray-2": error.length === 0
   });
 
   // Validation :
+  let error = "";
+
   const validate = (): string | null => {
-    return "";
+    error = "";
+    for (const validator of validators) {
+      const res = validator(value, required);
+      if (res.length > 0) {
+        error = res;
+        break;
+      }
+    }
+    return error;
   }
 
   const context = getContext<FormContext>("validator");
@@ -53,9 +69,9 @@
 
 <label>
   {#if label}
-    <div class="mb-1">
-      <Text type="p" size="normal" fontWeight="medium">{label}</Text>
+    <div class={`mb-1 ${error.length > 0 ? "text-error-1" : "text-black"}`}>
+      <Text type="p" size="normal" fontWeight="medium" color="inherit">{label}</Text>
     </div>
   {/if}
-  <input bind:this={inputRef} {placeholder} {type} {required} {value} {disabled} {name} class={style}>
+  <input bind:this={inputRef} {placeholder} bind:value {disabled} {name} class={style}>
 </label>
--- src/libraries/utils/validator.ts
+++ src/libraries/utils/validator.ts
@@ -1,9 +1,25 @@
 import { z } from "zod";
 
-export const isString = (value: unknown): boolean => {
-  return z.string().safeParse(value).success;
+export const isString = (value: string | number | null, required: boolean): string => {
+  if (typeof value === "string" && value.length === 0) value = null;
+  if (!value && !required) return "";
+
+  const parse = z.string().safeParse(value);
+  return parse.success ? "" : parse.error.message;
 }
 
-export const isNumber = (value: unknown): boolean => {
-  return z.number().safeParse(value).success;
+export const isNumber = (value: string | number | null, required: boolean): string => {
+  if (typeof value === "string" && typeof parseInt(value) === "number") value = parseInt(value);
+  if (!value && !required) return "";
+
+  const parse = z.number().safeParse(value);
+  return parse.success ? "" : parse.error.message;
+}
+
+export const isEmail = (value: string | number | null, required: boolean): string => {
+  if (typeof value === "string" && value.length === 0) value = null;
+  if (!value && !required) return "";
+
+  const parse = z.string().email().safeParse(value);
+  return parse.success ? "" : parse.error.message;
 }
