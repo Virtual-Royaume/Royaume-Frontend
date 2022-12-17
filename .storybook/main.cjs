cc5159819f9fa7a643c5a2a2223079dce420a569
Bluzzi <camilledugas.pro@gmail.com>
2022-12-17 11:55:30 +0100

    Setup Storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,24 +1,18 @@
-const preprocess = require("svelte-preprocess");
-const path = require("path");
-
 module.exports = {
-  "stories": ["../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-svelte-csf",
-    {
-      name: "@storybook/addon-postcss",
-      options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
-        },
-      },
-    }
+    "@storybook/addon-interactions"
   ],
   "framework": "@storybook/svelte",
   "core": {
     "builder": "@storybook/builder-vite"
   },
-  svelteOptions: import("../svelte.config.js").preprocess
+  "features": {
+    "storyStoreV7": true
+  }
 }
