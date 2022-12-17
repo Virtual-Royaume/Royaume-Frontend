9aada3027fdfae076ec7c870ae740ad4e33ece20
Bluzzi <camilledugas.pro@gmail.com>
2022-12-17 11:43:07 +0100

    Use TS for Svelte config

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,9 +1,9 @@
 import adapter from "@sveltejs/adapter-node";
-import preprocess from "svelte-preprocess";
+import type { Config } from "@sveltejs/kit";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
-/** @type {import("@sveltejs/kit").Config} */
-const config = {
-  preprocess: preprocess({
+const config: Config = {
+  preprocess: vitePreprocess({
     postcss: true
   }),
 
