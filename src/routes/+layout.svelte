22425987490b918b9e9b7e11097568810d2498a8
Bluzzi <camilledugas.pro@gmail.com>
2023-01-20 05:48:21 +0100

    Fix Tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import "$lib/styles/tailwind.css";
+</script>
+
+<main class="h-screen bg-black">
+  <slot />
+</main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ["./src/**/*.{html,svelte,ts}"],
 
   theme: {
     colors: {
