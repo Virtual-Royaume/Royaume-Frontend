93e222bcd342e4aa0d8eb4ea1be4857a9aa42abe
Bluzzi <camilledugas.pro@gmail.com>
2023-01-24 02:15:22 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="h-full flex items-center justify-center flex-col">
-  <img src="/favicon.png" alt="Royaume logo" class="animate-pulse h-64">
+  <img src="/favicon.png" alt="Royaume logo" class="h-64">
 
   <a class="flex items-center gap-2 text-white text-xl hover:brightness-90" href="./discord">
     <span>LE ROYAUME</span>
