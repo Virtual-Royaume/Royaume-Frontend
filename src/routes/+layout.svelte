5ff436c346286be13d3c59ae19937dbab890de92
Bluzzi <camilledugas.pro@gmail.com>
2023-03-12 23:40:35 +0100

    / (layout): use notification component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import { Notification } from "$lib/components/special/notification";
   import "$lib/styles/tailwind.css";
 </script>
 
 <main class="h-screen bg-gradient-to-r from-primary-1 to-primary-2">
   <slot />
 </main>
+
+<Notification />
