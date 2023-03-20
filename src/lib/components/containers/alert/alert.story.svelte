ca550d39f6a6ca12488f9d73f90d74fe6c9457ef
Gaëtan H <steellgold0@gmail.com>
2023-03-20 14:10:32 +0100

    📗 Container components `Storybook` to `Histoire`

---
+++ src/lib/components/containers/alert/alert.story.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import { Alert } from ".";
+  import type { AlertType } from './alert.type';
+  
+  export let Hst: Hst;
+
+  export let type: AlertType = 'info';
+  export let message: string = 'This is an alert message';
+</script>
+
+<Hst.Story title="Containers/Alert">
+  <svelte:fragment slot="controls">
+    <Hst.Text title="Texte" bind:value={message} />
+    <Hst.Select title="Type" bind:value={type} options={['info', 'success', 'warning', 'error']} />
+  </svelte:fragment>
+
+  <Hst.Variant>
+    <Alert message={message} type={type} />
+  </Hst.Variant>
+</Hst.Story>
