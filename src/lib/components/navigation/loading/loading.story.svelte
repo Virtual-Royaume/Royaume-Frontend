2531ecc6bae0e8ce7a2c7bb4e9a72f2e30c7a505
Gaëtan H <steellgold0@gmail.com>
2023-03-20 17:48:01 +0100

    📗 Loading component to Histoire

---
+++ src/lib/components/navigation/loading/loading.story.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import { Loading } from ".";
+  import type { LoadingType } from './loading.type';
+  
+  export let Hst: Hst;
+
+  export let type: LoadingType = 'primary';
+</script>
+
+<Hst.Story title="Navigation/Loading">
+  <svelte:fragment slot="controls">
+    <Hst.Select title="Type" bind:value={type} options={["primary", "secondary", "success", "warning", "error", "normal"]} />
+  </svelte:fragment>
+
+  <Hst.Variant>
+    <Loading color={type} />
+  </Hst.Variant>
+</Hst.Story>
