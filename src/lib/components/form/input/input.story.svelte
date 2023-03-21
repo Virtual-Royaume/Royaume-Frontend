7b69dd36032593665ebbbb6ca7da18e45ec2d798
Gaëtan H <steellgold0@gmail.com>
2023-03-21 14:40:11 +0100

    🪄 ESLint fix

--- src/lib/components/form/input/input.story.svelte
+++ src/lib/components/form/input/input.story.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-  import type { Hst } from '@histoire/plugin-svelte'
+  import type { Hst as Histoire } from "@histoire/plugin-svelte";
   import { Input } from ".";
-  import type { InputColor, InputSize, InputType } from './input.type';
+  import type { InputColor, InputSize, InputType } from "./input.type";
   
   export let Hst: Histoire;
 
   export let color: InputColor = "primary";
   export let size: InputSize = "normal";
   export let type: InputType = "text";
-  export let label: string = "Label";
-  export let placeholder: string = "Placeholder";
+  export let label = "Label";
+  export let placeholder = "Placeholder";
 </script>
 
 <Hst.Story title="Form/Input">
   <svelte:fragment slot="controls">
     <Hst.Text title="Label" bind:value={label} />
     <Hst.Text title="Placeholder" bind:value={placeholder} />
-    <Hst.Select title="Color" bind:value={color} options={['primary', 'secondary', 'error', 'success', 'warning']} />
-    <Hst.Select title="Size" bind:value={size} options={['small', 'normal', 'large']} />
-    <Hst.Select title="Type" bind:value={type} options={['text', 'password', 'email']} />
+    <Hst.Select title="Color" bind:value={color} options={["primary", "secondary", "error", "success", "warning"]} />
+    <Hst.Select title="Size" bind:value={size} options={["small", "normal", "large"]} />
+    <Hst.Select title="Type" bind:value={type} options={["text", "password", "email"]} />
   </svelte:fragment>
 
   <Hst.Variant title="Default">
