aa4b7d09a2d33a140563cec7915bd434ad8690b5
Gaëtan H <steellgold0@gmail.com>
2023-03-21 13:59:12 +0100

    📗 Checkbox component to Histoire

---
+++ src/lib/components/form/checkbox/checkbox.story.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import { Checkbox } from ".";
+  import type { CheckboxColor } from './checkbox.type';
+  
+  export let Hst: Hst;
+
+  export let color: CheckboxColor = "primary";
+  export let label: string = "Label";
+</script>
+
+<Hst.Story title="Form/Checkbox">
+  <svelte:fragment slot="controls">
+    <Hst.Text title="Label" bind:value={label} />
+    <Hst.Select title="Color" bind:value={color} options={['primary', 'secondary', 'error', 'success', 'warning']} />
+  </svelte:fragment>
+
+  <Hst.Variant title="Default">
+    <Checkbox color={color} label={label} />
+  </Hst.Variant>
+</Hst.Story>
