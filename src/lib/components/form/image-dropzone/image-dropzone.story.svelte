5591333f224cd83d0fe7c99bde7ca4a1de71e5f3
Gaëtan H <steellgold0@gmail.com>
2023-03-21 13:54:27 +0100

    📗 Image Dropzone to Histoire

---
+++ src/lib/components/form/image-dropzone/image-dropzone.story.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { Hst } from "@histoire/plugin-svelte";
+  import { ImageDropzone } from ".";
+  import type { ImageDropzoneColor, ImageDropzoneSize } from "./image-dropzone.type";
+  
+  export let Hst: Hst;
+
+  export let color: ImageDropzoneColor = "primary";
+  export let size: ImageDropzoneSize = "normal";
+</script>
+
+<Hst.Story title="Form/Input">
+  <svelte:fragment slot="controls">
+    <Hst.Select title="Color" bind:value={color} options={["primary", "secondary", "error", "success", "warning"]} />
+    <Hst.Select title="Size" bind:value={size} options={["small", "normal", "large"]} />
+  </svelte:fragment>
+
+  <Hst.Variant title="Default">
+    <ImageDropzone color={color} size={size} />
+  </Hst.Variant>
+</Hst.Story>
