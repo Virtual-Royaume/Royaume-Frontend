47ac6df650b80af259e56a785f071daa84b32e3a
Gaëtan H <steellgold0@gmail.com>
2023-03-21 13:59:21 +0100

    📗 Autocomplete Input component to Histoire

---
+++ src/lib/components/form/autocomplete-input/autocomplete-input.story.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Hst } from "@histoire/plugin-svelte";
+  import { AutocompleteInput } from ".";
+  import type { AutocompleteInputColor, AutocompleteSize } from "./autocomplete-input.type";
+  
+  export let Hst: Hst;
+
+  export let color: AutocompleteInputColor = "primary";
+  export let size: AutocompleteSize = "normal";
+  export let label: string = "Label";
+  export let placeholder: string = "Placeholder";
+</script>
+
+<Hst.Story title="Form/ImageDropzone">
+  <svelte:fragment slot="controls">
+    <Hst.Text title="Label" bind:value={label} />
+    <Hst.Text title="Placeholder" bind:value={placeholder} />
+    <Hst.Select title="Color" bind:value={color} options={["primary", "secondary", "error", "success", "warning"]} />
+    <Hst.Select title="Size" bind:value={size} options={["small", "normal", "large"]} />
+  </svelte:fragment>
+
+  <Hst.Variant title="Default">
+    <AutocompleteInput color={color} size={size} label={label} placeholder={placeholder} />
+  </Hst.Variant>
+</Hst.Story>
