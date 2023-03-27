fea208a52b755299a4d30750da0b5799d60a01e7
Bluzzi <camilledugas.pro@gmail.com>
2023-03-27 18:52:19 +0200

    histoire (config): update theme

--- histoire.config.ts
+++ histoire.config.ts
@@ -13,13 +13,29 @@ export default defineConfig({
   ],
 
   theme: {
+    hideColorSchemeSwitch: true,
+    defaultColorScheme: "dark",
     logo: {
       dark: "./static/images/royaume-logo-text-white.png",
-      light: "./static/images/royaume-logo-text-black.png",
-      square: "./static/images/royaume-logo-text-white.png"
+      square: "./static/images/royaume-logo.png"
     },
     colors: {
-      primary: defaultColors.green
+      primary: defaultColors.green,
+      gray: {
+        "50": "#F9FAFB",
+        "100": "#F3F4F6",
+        "200": "#E5E7EB",
+        "300": "#D1D5DB",
+        "400": "#9CA3AF",
+        "500": "#6B7280",
+        "600": "#4B5563",
+        "700": "#0C0C0D",
+        "750": "#0A0A0B",
+        "800": "#060606",
+        "850": "#040404",
+        "900": "#020202",
+        "950": "#010101"
+      }
     }
   }
 });
