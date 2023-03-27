1f0cdf2782861f425542ac1db7558e5a184787c1
Bluzzi <camilledugas.pro@gmail.com>
2023-03-27 16:08:48 +0200

    histoire (config): update theme

--- histoire.config.ts
+++ histoire.config.ts
@@ -1,12 +1,25 @@
-import { defineConfig } from "histoire";
+import { defaultColors, defineConfig } from "histoire";
 import { HstSvelte } from "@histoire/plugin-svelte";
 
 export default defineConfig({
   plugins: [
     HstSvelte()
   ],
+
   setupFile: "./histoire.setup.ts",
+
   viteNodeInlineDeps: [
     /clsx/
-  ]
+  ],
+
+  theme: {
+    logo: {
+      dark: "./static/images/royaume-logo-text-white.png",
+      light: "./static/images/histoire-logo.svg",
+      square: "./static/images/histoire-logo.svg"
+    },
+    colors: {
+      primary: defaultColors.green
+    }
+  }
 });
