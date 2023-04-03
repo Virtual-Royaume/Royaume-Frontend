6f77fbaabd91899d7a2f9ca5f11a70001a992eed
Gaëtan H <steellgold0@gmail.com>
2023-04-03 12:28:35 +0200

    Navbar opacity change

--- src/lib/components/layouts/navbar/navbar.svelte
+++ src/lib/components/layouts/navbar/navbar.svelte
@@ -1,4 +1,4 @@
-<div class="p-2 bg-[#000000]/75 fixed h-16 w-full">
+<div class="p-2 bg-[#000000]/50 fixed h-16 w-full">
   <div class="flex mt-2 items-center justify-between mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div class="icon">
       <a href="/">
