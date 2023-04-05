2d2f8fdb525b2e6dca2f011ff680d654a1cd51d5
Gaëtan H <gaetanhus@gmail.com>
2023-04-05 14:42:22 +0200

    icons: discord from tabler-icons

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -9,3 +9,4 @@ export { default as WarningIcon } from "./list/warning.svelte";
 export { default as ExternalIcon } from "./list/external.svelte";
 export { default as AlertCircle } from "./list/alert-circle.svelte";
 export { default as Clipboard } from "./list/clipboard.svelte";
+export { default as BrandDiscord } from "./list/discord.svelte";
