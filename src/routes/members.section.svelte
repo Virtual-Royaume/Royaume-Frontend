2e2bc693e8d402ba111250547accfa56b43239cd
Gaëtan H <gaetanhus@gmail.com>
2023-04-05 13:27:19 +0200

    / (members) : username/hover + speedy adapt mobile

---
+++ src/routes/members.section.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import data from "../temp/members.json";
+  interface Member {
+    profilePicture: string;
+    username: string;
+  }
+
+  export let members: Member[] = [];
+
+  for (let i = 0; i < data.data.members.length; i++) {
+
+    if (data.data.members[i].profilePicture == "https://i.ytimg.com/vi/Ug9Xh-xNecM/maxresdefault.jpg") {
+      continue;
+    }
+
+    members.push({
+      profilePicture: data.data.members[i].profilePicture,
+      username: data.data.members[i].username
+    });
+  }
+</script>
+
+<div class="mt-16 sm:mx-auto sm:w-4/5">
+  <div class="grid [grid-template-areas:'stack']">
+    <div class="[grid-area:stack] grid grid-cols-[repeat(4,1fr)] md:grid-cols-[repeat(20,1fr)] z-10">
+      {#each members as member, i}
+        <div class="odd:translate-y-4 even:-translate-y-4 flex flex-col justify-center gap-1">
+          <img src="{member.profilePicture}" class="w-16 h-16 rounded-full hover:border-[2px] hover:border-[#39279C]" alt="{member.username}">
+        </div>
+      {/each}
+    </div>
+    <div class="[grid-area:stack z-40"></div>
+  </div>
+</div>
