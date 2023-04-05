ddd25da958e4e556d2e3ed553187489a38493a76
Gaëtan H <gaetanhus@gmail.com>
2023-04-05 13:27:33 +0200

    / (home) move to sections

---
+++ src/routes/stacks.section.svelte
@@ -0,0 +1,52 @@
+<div class="flex flex-col justify-center items-center mt-10">
+  <div class="justify-center items-center flex flex-col mt-10 basis-1/2">
+    <h3 class="font-black text-white md:text-xl">DÉCOUVREZ NOS SPÉCIALITÉES</h3>
+    <p class="font-normal text-center text-white w-4/5 mt-1 md:text-normal">Voici ce qu'on sait faire de mieux chez nous, et si vous ne savez pas, ce n'est pas un soucis on peut vous apprendre et vous tenir la main !</p>
+  </div>
+
+  <!-- 3 grids -->
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 mt-10">
+    <div class="flex flex-row justify-center items-center">
+      <div class="flex flex-col justify-center items-center mr-5 bg-[#151517] rounded-lg w-64 h-64 p-5 border-t-[3px] border-[#39279C]">
+        <div class="bg-blue-500 rounded-full w-52 flex justify-center items-center">
+          <img src="/images/presentation/svelte-kit.png" alt="SvelteKit" class="mt-4 md:mt-0">
+        </div>
+        <h3 class="font-normal text-[#999999] md:text-large mt-5">SvelteKit</h3>
+        <p class="font-normal text-center text-[#626262] w-4/3 mt-1 md:text-normal">Nous avons des spécialistes qui adorent travailler avec cette technologie du futur !</p>
+
+        <a href="/projects?type=sveltekit" class="bg-[#1D1D1F] text-[#626262] text-normal py-2 w-36 text-center rounded-lg mt-5">
+          Décrouvrir
+        </a>
+      </div>
+    </div>
+
+    <div class="flex flex-row justify-center items-center">
+      <div class="flex flex-col justify-center items-center mr-5 bg-[#151517] rounded-lg w-64 h-64 p-5 border-t-[3px] border-[#39279C]">
+        <div class="bg-blue-500 rounded-full w-52 flex justify-center items-center">
+          <img src="/images/presentation/react.png" alt="React" class="w-16 mt-4 md:-mt-6">
+        </div>
+        <h3 class="font-normal text-[#999999] md:text-large mt-5">React</h3>
+        <p class="font-normal text-center text-[#626262] w-4/3 mt-1 md:text-normal">On a des professionnels du domaine qui sauront vous aider dans n’importe quel contexte</p>
+
+        <a href="/projects?type=react" class="bg-[#1D1D1F] text-[#626262] text-normal py-2 w-36 text-center rounded-lg mt-5">
+          Décrouvrir
+        </a>
+      </div>
+    </div>
+
+    <div class="flex flex-row justify-center items-center">
+      <div class="flex flex-col justify-center items-center mr-5 bg-[#151517] rounded-lg w-64 h-64 p-5 border-t-[3px] border-[#39279C]">
+        <div class="bg-blue-500 rounded-full w-52 flex justify-center items-center">
+          <img src="/images/presentation/supabase.png" alt="Supabase" class="w-16 mt-6 md:-mt-6">
+        </div>
+        <h3 class="font-normal text-[#999999] md:text-large mt-5">Supabase</h3>
+        <p class="font-normal text-center text-[#626262] w-4/3 mt-1 md:text-normal">Ce n’est pas qu’une base de données, c’est une plateforme qui révolutionne ce qu’on connais</p>
+
+        <!-- Fixed to the top of card -->
+        <a href="/projects?type=supabase" class="bg-[#1D1D1F] text-[#626262] text-normal py-2 w-36 text-center rounded-lg mt-5">
+          Décrouvrir
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
