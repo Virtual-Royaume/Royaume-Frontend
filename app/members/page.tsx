c026cefe74f8b0a65be2c96bcdc54c386916e516
Gaëtan H <steellgold0@gmail.com>
2023-06-13 21:28:36 +0200

    style(members): update page title and member card width
    - Update page title to use Heading component and change it to h2
    - Update member card width to be wider on larger screens
    
    This commit improves the visual design of the members page by updating the
    page title to use the Heading component and changing it to h2. It also updates
    the width of the member card to be wider on larger screens, improving the
    layout of the page.

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,5 +1,6 @@
 import { MemberCard } from "@lib/components/atomics/cards/member-card";
 import { Text } from "@lib/components/atomics/texts";
+import { Heading } from "@lib/components/atomics/texts/heading";
 import { members } from "@lib/configs/members";
 import { ReactElement } from "react";
 
@@ -7,8 +8,8 @@ const MembersPage = (): ReactElement => {
   return (
     <>
       <div className="flex flex-col items-center justify-center mt-20">
-        <Text about="h1" className="text-3xl font-semibold">Découvrez nos membres</Text>
-        <Text about="p" className="text-white-desc text-center mt-4">Nos membres sont des personnes qui ont contribué au projets, ou juste des membres de la communauté.</Text>
+        <Heading type="h2" className="px-2 text-center font-bold">Découvrez nos membres</Heading>
+        <Text about="p" className="px-2 text-center">Nos membres sont des personnes qui ont contribué au projets, ou juste des membres de la communauté.</Text>
       </div>
 
       <div className="flex flex-wrap justify-center mt-10 gap-5">
--- lib/components/atomics/cards/member-card/member-card.tsx
+++ lib/components/atomics/cards/member-card/member-card.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export const MemberCard: Component<MemberCardProps> = ({ username, image, birthday }) => {
   return (
-    <div className="bg-background-card px-2 py-3 rounded-md flex gap-5 items-center w-72">
+    <div className="bg-background-card px-2 py-3 rounded-md flex gap-5 items-center w-96 sm:w-72">
       <div className="flex items-center gap-2">
         <Image src={image} alt="crown" width={60} height={60} className="rounded-md" />
       </div>
