e0abdd834679ff960c287904eb3a667e1d667a5e
RomainSav <romain.sv@icloud.com>
2023-06-13 22:40:39 +0200

    Change of style to match model

--- lib/components/atomics/cards/member-card/member-card.tsx
+++ lib/components/atomics/cards/member-card/member-card.tsx
@@ -2,22 +2,37 @@ import type { Component } from "@lib/utils/component";
 import type { MemberCardProps } from "./member-card.type";
 import Image from "next/image";
 import { DayJS } from "@lib/utils/day-js";
+import { CiLocationOn } from "react-icons/ci";
 
 export const MemberCard: Component<MemberCardProps> = ({ username, profilePicture, birthday }) => {
   return (
-    <div className="bg-background-card p-3 rounded-md flex gap-5 items-center w-full">
-      <div className="flex items-center gap-2">
-        <Image src={profilePicture} alt="crown" width={60} height={60} className="rounded-md" />
+    <div className="bg-background-card p-3 rounded-md w-full flex gap-5">
+      <div className="relative h-32 aspect-square flex items-center gap-2">
+        <Image src={profilePicture} alt="crown" fill className="rounded-md" />
       </div>
 
-      <div className="flex items-center gap-1 items-baseline">
-        <p className="text-white text-lg">{username}</p>
+      <div className="relative w-full">
+        <div className="flex items-center gap-2 items-baseline mb-1">
+          <p className="text-white text-lg">{username}</p>
 
-        {birthday && (
+          {birthday && (
             <p className="text-white-desc text-xs">
               {DayJS().diff(birthday, "year", false)} ans
             </p>
           )}
+        </div>
+
+        <div className="text-white-desc flex items-center gap-1">
+          <CiLocationOn />
+          <p className="text-sm">France</p>
+        </div>
+
+        <div className="absolute bottom-0 w-full">
+          <p className="text-sm text-white-desc mb-1">Technologies utilisées</p>
+          <div className="h-6 w-full rounded-md bg-background-info">
+
+          </div>
+        </div>
       </div>
     </div>
   );
