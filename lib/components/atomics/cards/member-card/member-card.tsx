5ccdbe3feb73dc2a06f29142a484463eab79ae83
Gaëtan H <steellgold0@gmail.com>
2023-06-13 21:19:29 +0200

    feat(member-card.tsx): replace img tag with Next.js Image component for better performance and accessibility

--- lib/components/atomics/cards/member-card/member-card.tsx
+++ lib/components/atomics/cards/member-card/member-card.tsx
@@ -1,12 +1,13 @@
 import { Component } from "@lib/utils/component";
 import { MemberCardProps } from "./member-card.type";
 import { DayJS } from "@lib/utils/day-js";
+import Image from "next/image";
 
 export const MemberCard: Component<MemberCardProps> = ({ username, image, birthday }) => {
   return (
     <div className="bg-background-card px-2 py-3 rounded-md flex gap-5 items-center w-72">
       <div className="flex items-center gap-2">
-        <img src={image} alt={username} className="rounded-md w-16 h-16" />
+        <Image src={image} alt="crown" width={60} height={60} className="rounded-md" />
       </div>
 
       <div className="flex flex-col gap-1">
