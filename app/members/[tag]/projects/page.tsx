85cec3a3fa356eec16688174e2d6de1905378017
RomainSav <romain.sv@icloud.com>
2023-06-15 17:47:55 +0200

    Update page.tsx

--- app/members/[tag]/projects/page.tsx
+++ app/members/[tag]/projects/page.tsx
@@ -4,7 +4,6 @@ import { useContext, type ReactElement } from "react";
 import { notFound } from "next/navigation";
 import { MemberContext } from "../member-provider";
 import { Heading } from "@lib/components/atomics/texts/heading";
-import { NewProjectCard } from "@lib/components/atomics/cards/project-cards/new-project-card";
 import Image from "next/image";
 
 const MemberPage = (): ReactElement => {
@@ -15,45 +14,43 @@ const MemberPage = (): ReactElement => {
     <div>
       {member.projects && member.projects.length > 0 ? (
         <div className="bg-background-card p-4 rounded-md">
-          <>
-            <div className="grid gap-1">
-              {member.projects.map((project) => (
-                <div className="text-white-desc items-center gap-2">
-                  <div className="flex items-center gap-2">
-                    <Heading type="h4" className="text-white">{project.name}</Heading>
-                    <div className="px-2 pb-2 rounded-md bg-[#0c0c0d]">
-                      {project.technologies && (
-                        <div className="flex gap-2">
-                          {project.technologies.map((technology) => (
-                            <div className="flex items-center gap-1 mt-2">
-                              <Image src={`/images/icons/${technology.icon}`} alt={`${technology.name} Icon`} height={15} width={15} />
-                            </div>
-                          ))}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                  <div className="h-1 w-8 bg-purple rounded-md mb-2" />
-                    <p className="text-white-desc text-opacity-50">{project.description}</p>
-
-                    {project.images && (
+          <div className="grid gap-1">
+            {member.projects.map((project) => (
+              <div className="text-white-desc items-center gap-2">
+                <div className="flex items-center gap-2">
+                  <Heading type="h4" className="text-white">{project.name}</Heading>
+                  <div className="px-2 pb-2 rounded-md bg-[#0c0c0d]">
+                    {project.technologies && (
                       <div className="flex gap-2">
-                        {project.images.map((image) => (
+                        {project.technologies.map((technology) => (
                           <div className="flex items-center gap-1 mt-2">
-                            <Image
-                              src={`${image}`}
-                              alt={`${project.name} Image`}
-                              className="rounded-md w-full"
-                              height={10}
-                              width={1000}/>
+                            <Image src={`/images/icons/${technology.icon}`} alt={`${technology.name} Icon`} height={15} width={15} />
                           </div>
                         ))}
                       </div>
                     )}
                   </div>
-                ))}
+                </div>
+                <div className="h-1 w-8 bg-purple rounded-md mb-2" />
+                <p className="text-white-desc text-opacity-50">{project.description}</p>
+
+                {project.images && (
+                  <div className="flex gap-2">
+                    {project.images.map((image) => (
+                      <div className="flex items-center gap-1 mt-2">
+                        <Image
+                          src={`${image}`}
+                          alt={`${project.name} Image`}
+                          className="rounded-md w-full"
+                          height={10}
+                          width={1000} />
+                      </div>
+                    ))}
+                  </div>
+                )}
               </div>
-            </>
+            ))}
+          </div>
         </div>
       ) : (
         <div className="text-white-desc flex items-center gap-2 border-l-4 border-l-danger pl-2">
