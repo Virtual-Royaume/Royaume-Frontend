6a4cfc1e0f543d8e6c51dae450b475243a30fa65
Bluzzi <camilledugas.pro@gmail.com>
2023-06-16 01:08:15 +0200

    style(tailwind.config.ts): reformat file to improve readability and consistency of code style feat(tailwind.config.ts): add new borderRadius values to be used in components feat(tailwind.config.ts): add new gradient-conic and gradient-radial background images to be used in components feat(tailwind.config.ts): add new gridTemplateColumns values to be used in components feat(tailwind.config.ts): add new colors to be used in components, including transparent, discord, and background colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,31 @@
 import type { Config } from "tailwindcss";
 
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.tsx",
+    "./lib/**/*.tsx"
   ],
+
   theme: {
-    container: {
-      center: true,
-      padding: "2rem"
+    fontFamily: {
+      "sans": ["Inter", "sans-serif"]
     },
+
     colors: {
+      transparent: "transparent",
+
+      danger: "#DD4439",
+      white: {
+        DEFAULT: "#FFFFFF",
+        desc: "#AFAFAF"
+      },
+      purple: {
+        DEFAULT: "#5339DD",
+        glow: "rgba(83, 57, 221, .52)"
+      },
+
+      discord: "#5865F2",
+
       background: {
         DEFAULT: "#0c0c0d",
         header: "rgba(12, 12, 13, .5)",
@@ -20,36 +34,36 @@ export default {
           hover: "#1E1E1F"
         },
         info: "#1E1E1F"
-      },
-      white: {
-        DEFAULT: "#FFFFFF",
-        desc: "#AFAFAF"
-      },
-      discord: "#5865F2",
-      purple: {
-        DEFAULT: "#5339DD",
-        glow: "rgba(83, 57, 221, .52)"
-      },
-      danger: "#DD4439"
+      }
     },
-    fontFamily: {
-      "sans": ["Inter", "sans-serif"]
+
+    borderRadius: {
+      DEFAULT: "0.375rem",
+      full: "9999px"
     },
+
+    container: {
+      center: true,
+      padding: "2rem"
+    },
+
     boxShadow: {
       glow: "0 0 200px 100px #5339DD"
     },
+
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
       },
+
       gridTemplateColumns: {
-        members: "repeat(auto-fill, minmax(5rem, 1fr))",
+        "members": "repeat(auto-fill, minmax(5rem, 1fr))",
         "members-cards": "repeat(auto-fill, minmax(20rem, 1fr))",
         "posts-cards": "repeat(auto-fill, minmax(18rem, 1fr))"
       }
     }
   },
+
   plugins: []
 } satisfies Config;
