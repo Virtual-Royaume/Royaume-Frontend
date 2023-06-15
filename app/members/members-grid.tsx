bcc5703625bdd4fd53a9e118572786986f51ffb4
Gaëtan H <steellgold0@gmail.com>
2023-06-15 11:05:14 +0200

    feat(members-grid.tsx): add search functionality to filter members by username or tag

--- app/members/members-grid.tsx
+++ app/members/members-grid.tsx
@@ -17,8 +17,31 @@ export const MembersGrid = (): ReactElement => {
     setMembersData(members.slice((page - 1) * itemPerPages, itemPerPages * page));
   }
 
+  const [search, setSearch] = useState("");
+  
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+
+    if (e.target.value === "") {
+      setMembersData(members.slice(0, itemPerPages));
+      setCurrentPage(1);
+    }
+
+    const filteredMembers = members.filter((member) => 
+      member.username.toLowerCase().includes(e.target.value.toLowerCase()) ||
+      member.tag.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+
+    setMembersData(filteredMembers.slice(0, itemPerPages));
+    setCurrentPage(1);
+  }
+
   return (
     <div className="mt-10">
+      <div className="flex justify-start mb-5">
+        <input type="text" placeholder="Rechercher un membre" className="w-96 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent" value={search} onChange={handleSearch} />
+      </div>
+      
       <div className="grid md:grid-cols-members-cards gap-5 mb-14">
         {membersData.sort((a, b) => a.username < b.username ? -1 : 1).map((member) => (
           <MemberCard key={member.username} {...member} />
