f9172b6dc492ca557c703f01260380fab7bf12fd
RomainSav <romain.sv@icloud.com>
2023-06-15 19:25:59 +0200

    Update blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,25 @@
+import { Text } from "@lib/components/atoms/texts";
+import { Heading } from "@lib/components/atoms/texts/heading";
+import Image from "next/image";
 import type { ReactElement } from "react";
 
 const HomePage = (): ReactElement => {
   return (
-    <>
-      <p className="text-white font-medium text-lg items-center justify-center flex">
-        Hello world this is a blank page for the blog
-      </p>
-    </>
+    <div className="container">
+      <div className="grid grid-cols-1 lg:grid-cols-2 items-center mt-28">
+        <div className="flex flex-col justify-center order-last lg:order-first">
+          <Heading type="h1" className="font-bold uppercase">Blog</Heading>
+          <Text>Découvrez de nouvelles technologies ou apprenez-en sur l'actualité. Vous trouverez des potes réguliers rédigés par les soins de nos membres.</Text>
+        </div>
+
+        <div className="justify-self-start lg:justify-self-end relative">
+          <div className="relative z-20 h-36 w-52 mb-4 lg:mb-0 lg:h-72 lg:w-96 lg:mr-16">
+            <Image alt="crown" src="/images/crown.png" className="relative z-10 object-contain" fill />
+          </div>
+          <span className="absolute -top-32 right-0 h-[35rem] w-[35rem] rounded-full blur-3xl opacity-30 right-0 bg-purple rounded-full" />
+        </div>
+      </div>
+    </div>
   )
 };
 
