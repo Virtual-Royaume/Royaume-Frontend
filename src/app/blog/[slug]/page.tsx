c8d614fb394fb2937b0091b589562847ce302876
RomainSav <romain.sv@icloud.com>
2023-06-16 19:25:46 +0200

    Introduce blog post page

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import type { BlogPost } from "#/lib/configs/blog/blog.type";
+import type { Component } from "#/lib/utils/component";
+import type { BlogPostPageProps } from "./page.type";
+import { notFound } from "next/navigation";
+import { Heading } from "#/lib/components/atoms/texts/heading";
+import { posts } from "#/lib/configs/blog";
+import Image from "next/image";
+import type { Metadata } from "next";
+
+const getPost = (slug: string): BlogPost | null => {
+  return posts.find((post) => post.slug === slug) ?? null;
+};
+
+export const generateMetadata = ({ params }: BlogPostPageProps): Metadata => {
+  const post = getPost(params.slug);
+  if (!post) notFound();
+
+  return {
+    title: post.title,
+    description: post.description,
+
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      images: [post.thumbnail]
+    }
+  };
+};
+
+const BlogPostPage: Component<BlogPostPageProps> = ({ params }) => {
+  const post = getPost(params.slug);
+  if (!post) notFound();
+
+  return (
+    <div className="mt-28 container">
+      <div className="relative mx-auto lg:w-4/6 aspect-video">
+        <Image src={post.thumbnail} alt="Post Thumbnail" fill className="object-cover rounded" />
+      </div>
+
+      <Heading type="h1" className="uppercase text-center mt-10 text-3xl lg:text-5xl">{post.title}</Heading>
+    </div>
+  );
+};
+
+export default BlogPostPage;
