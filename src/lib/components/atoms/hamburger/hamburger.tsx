8d14c7d838be81697d8671c4d60bc07a245d1959
Bluzzi <camilledugas.pro@gmail.com>
2023-06-16 14:30:58 +0200

    refactor(hamburger.tsx): extract common styles to a separate variable to improve readability style(hamburger.tsx): change button size to 10x10 and icon size to 10x10 to improve consistency with other components feat(hamburger.tsx): add transition effect to icon opacity to improve user experience feat(hamburger.tsx): add cursor pointer to button and icons to improve user experience feat(hamburger.tsx): add styles to iconStyles variable to improve readability and consistency

--- src/lib/components/atoms/hamburger/hamburger.tsx
+++ src/lib/components/atoms/hamburger/hamburger.tsx
@@ -1,25 +1,28 @@
 import type { Component } from "#/lib/utils/component";
 import type { HamburgerProps } from "./hamburger.type";
-import { clsx } from "clsx";
 import { FiMenu, FiX } from "react-icons/fi";
+import { clsx } from "clsx";
 
 export const Hamburger: Component<HamburgerProps> = ({ open, setOpen }) => {
-  // Common styles :
-  const styles = clsx(
-    "w-full h-full text-white",
+  // Styles :
+  const styles = "w-10 h-10";
+  const iconStyles = clsx(
+    "text-white",
     "absolute right-0 top-0 bottom-0 my-auto",
     "transition-opacity duration-300",
-    "cursor-pointer"
+    "cursor-pointer",
+    styles
   );
 
+  // Render:
   return (
     <button
-      className="h-12 w-12 relative appearance-none"
+      className={clsx("relative appearance-none", styles)}
       onClick={() => setOpen((state) => !state)}
       aria-label="hamburger"
     >
       <FiX className={clsx(
-        styles,
+        iconStyles,
         {
           "opacity-100": open,
           "opacity-0": !open
@@ -27,7 +30,7 @@ export const Hamburger: Component<HamburgerProps> = ({ open, setOpen }) => {
       )} />
 
       <FiMenu className={clsx(
-        styles,
+        iconStyles,
         {
           "opacity-0": open,
           "opacity-100": !open
