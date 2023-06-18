657bb987bbbba2dc6b544026bd7c383a4d6a72d3
Bluzzi <camilledugas.pro@gmail.com>
2023-06-19 00:53:52 +0200

    chore(package.json): add @testing-library/react and happy-dom dependencies test(debounce.test.ts): uncomment tests and fix indentation chore(vitest.config.ts): add happy-dom environment to tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     setupFiles: ["dotenv/config"],
-    include: ["./src/**/*.test.ts"]
+    include: ["./src/**/*.test.ts"],
+    environment: "happy-dom"
   },
   resolve: {
     alias: {
