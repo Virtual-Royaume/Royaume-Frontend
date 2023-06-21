677c41c048c797091253a99d9475f30860cb53db
RomainSav <romain.sv@icloud.com>
2023-06-22 01:14:11 +0200

    🚀 feat(input): add tests and state management for input component ✨ feat(input): add tests and state management for input component The input component now has tests that ensure it can be typed into and that it cannot be typed into when it is disabled. The component also now has state management for its value, which allows it to be controlled by its parent component.

--- src/lib/components/atoms/input/input.tsx
+++ src/lib/components/atoms/input/input.tsx
@@ -2,11 +2,12 @@
 
 import type { InputProps } from "./input.type";
 import { sm } from "#/lib/utils/style/class";
-import { forwardRef, useContext } from "react";
+import { forwardRef, useContext, useEffect, useState } from "react";
 import { LabelContext } from "../label/label-provider";
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(({ className, disabled, ...props }, ref) => {
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ className, disabled, value: valueProps, ...props }, ref) => {
   const haveError = useContext(LabelContext);
+  const [value, setValue] = useState(valueProps);
 
   const styles = sm(
     "outline-none bg-background-card border-2 text-white-desc rounded px-3 py-2 w-full",
@@ -27,6 +28,8 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(({ className, disa
     ref={ref}
     className={styles}
     disabled={disabled}
+    value={value}
+    onChange={(e) => setValue(e.target.value)}
     {...props} />;
 });
 
