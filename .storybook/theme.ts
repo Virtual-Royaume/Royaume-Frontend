b198bae6cef9e81dc1800149bbc6c269741d1553
RomainSav <romain.sv@icloud.com>
2023-06-26 22:22:55 +0200

    🎨 style(.storybook): add meta tags and styles to improve design system documentation ✨ feat(.storybook): add custom theme to Storybook manager UI 📦 chore(package.json): add @storybook/theming package as a dependency The meta tags and styles added to the manager-head.html file improve the design system documentation by providing a more complete and consistent experience for users. The custom theme added to the Storybook manager UI improves the branding and visual consistency of the design system. The @storybook/theming package is added as a dependency to enable the use of custom themes in Storybook.

---
+++ .storybook/theme.ts
@@ -0,0 +1,29 @@
+import { create } from "@storybook/theming/create";
+
+export const theme = create({
+  base: "dark",
+
+  // Typography :
+  fontBase: "'Inter', sans-serif",
+  fontCode: "Source Code Pro",
+
+  // Branding :
+  brandTitle: "Royaume Design System",
+  brandUrl: "https://royaume.world",
+  brandImage: "https://iili.io/HPLKcvf.png",
+  brandTarget: "_self",
+
+  // Colors :
+  colorPrimary: "#E862B3",
+  colorSecondary: "#6366F1",
+  
+  // Background colors :
+  appBg: "#131314",
+  appContentBg: "#0C0C0D",
+  appBorderColor: "#1E1E1F",
+  appBorderRadius: 4,
+
+  // Toolbar colors :
+  barBg: "#131314",
+  barSelectedColor: "#6366F1",
+});
